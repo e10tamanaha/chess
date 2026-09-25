97b374f2c4885c3d8f6d6c4d086e55cae893c86f
Ethan Tamanaha <e10.tamanaha@gmail.com>
2026-09-24 21:39:27 -0600

    Implemented getBoard()

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -140,7 +140,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 
     /**
